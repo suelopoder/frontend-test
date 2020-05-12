62bf0b8217e38f274e76fe5a8607e921023e7609
Diego Sisto <diego.sisto@xapo.com>
2020-05-12 10:08:40 -0300

    Simple client version. Needs a lot of refactors :)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const TEST_DOCS = [
+  { name: 'Doc 1', size: 999999 },
+  { name: 'Doc 2', size: 20 },
+  { name: 'Doc 3', size: 50000 },
+];
 function App() {
+  const [docs, setDocs] = useState(TEST_DOCS);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const addDoc = () => {
+    const random = Math.ceil(Math.random()*100);
+    setDocs([
+      ...docs,
+      { name: `Doc ${random}`, size: random * 1000 }
+    ]);
+  }
+
+  const displayedDocs = docs.filter(doc =>
+    doc.name.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase()));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <input
+          onChange={e => setSearchQuery(e.target.value)}
+          value={searchQuery}
+          placeholder="Seach documents..."
+        />
+      </section>
+      <section>
+        <button onClick={addDoc}>Upload</button>
+      </section>
+      <section>
+        docs
+        <ul>
+          {displayedDocs.map(doc =>
+            <li key={doc.name}>{doc.name}</li>
+          )}
+        </ul>
+      </section>
     </div>
   );
 }
