b5ca3c5ffdf5b0600490fa6b99a9b14b14a09ca2
Diego Sisto <diego.sisto@xapo.com>
2020-05-13 14:59:18 -0300

    Fix bug in size render

--- src/components/Document.js
+++ src/components/Document.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { getSizeInKb } from '../helpers';
 
 const Document = (props) => (
   <li className="document">
     <h3>{props.name}</h3>
-    <span>{getSizeInKb(props.size)}kb</span>
+    <span>{props.size}kb</span>
     <button onClick={props.onDelete}>delete</button>
   </li>
 );
