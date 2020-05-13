cd67de1691386bb22df39de050f7cc4d2043ef12
Diego Sisto <diego.sisto@xapo.com>
2020-05-13 13:11:11 -0300

    Add UI test for Upload component

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 import { VALID_FILE_EXTENSIONS } from '../constants';
 
 const Upload = ({ onUpload, isValid }) => {
@@ -31,6 +32,7 @@ const Upload = ({ onUpload, isValid }) => {
           accept={`file_extension:${VALID_FILE_EXTENSIONS.join(',')}`}
           style={{ display: 'none' }}
           ref={inputRef}
+          data-testid="upload-input"
           />
         <button onClick={onButtonClick}>Upload</button>
         {error && <span className="error">Valid files are jpeg and png files under 10MB</span>}
@@ -38,5 +40,10 @@ const Upload = ({ onUpload, isValid }) => {
     </section>
   );
 }
+
+Upload.propTypes = {
+  onUpload: PropTypes.func.isRequired,
+  isValid: PropTypes.func.isRequired,
+};
 
 export default Upload;
---
+++ src/components/Upload.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { render, fireEvent, cleanup, act } from '@testing-library/react';
+import Upload from './Upload';
+
+afterEach(cleanup);
+
+const FILE = new File(
+  ['(⌐□_□)'],
+  'chucknorris.png',
+  { type: 'image/png', size: 1000, name: 'chucknorris.png' }
+);
+
+test('callback is called when file is valid', () => {
+  const onUploadFn = jest.fn();
+
+  const { getByTestId } = render(<Upload onUpload={onUploadFn} isValid={() => null}/>)
+  const input = getByTestId('upload-input');
+  act(() => {
+    fireEvent.change(input, { target: { files: [FILE] } });
+  });
+
+  expect(onUploadFn.mock.calls.length).toBe(1);
+});
+
+test('callback is not called and error is shown for when file is invalid', () => {
+  const onUploadFn = jest.fn();
+
+  const { getByText, getByTestId } = render(<Upload onUpload={onUploadFn} isValid={() => 'some error'}/>)
+  const input = getByTestId('upload-input');
+  act(() => {
+    fireEvent.change(input, { target: { files: [FILE] } });
+  });
+
+  expect(onUploadFn.mock.calls.length).toBe(0);
+  expect(getByText('Valid files are jpeg and png files under 10MB')).toBeInTheDocument();
+});
+
+test('error is cleared after a valid file is submitted', () => {
+  const getFileError = file => {
+    if (file.name === 'chucknorris.png') {
+      return 'no chuch';
+    }
+    return null
+  };
+
+  const invalidFile = new File(
+    ['(⌐□_□)'],
+    'chucknorris.png',
+    { type: 'image/png' }
+  );
+  const { getByText, getByTestId, queryByText } = render(<Upload isValid={getFileError} onUpload={() => {}}/>)
+  const input = getByTestId('upload-input');
+
+  act(() => {
+    fireEvent.change(input, { target: { files: [invalidFile] } });
+  });
+
+  expect(getByText('Valid files are jpeg and png files under 10MB')).toBeInTheDocument();
+
+  const validFile = new File(
+    ['(⌐□_□)'],
+    'vandamme.png',
+    { type: 'image/png' }
+  );
+
+  act(() => {
+    fireEvent.change(input, { target: { files: [validFile] } });
+  });
+
+  expect(queryByText('Valid files are jpeg and png files under 10MB')).not.toBeInTheDocument();
+});
